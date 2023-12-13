17a1b61b8f0955a4c9c354b2d30de5c633b86d26
Norbert Falcó <norbertfalco@gmail.com>
2023-12-13 19:26:53 +0100

    team section acavat i retocs al CTA

--- main.js
+++ main.js
@@ -7,14 +7,50 @@ Alpine.start()
 
 document.addEventListener('DOMContentLoaded', function () {
     const spotlightDiv = document.getElementById('mySpotlightDiv');
+    const toggleButton = document.getElementById('toggleButton');
+    const spotlightOverlay = document.getElementById('spotlightOverlay');
+    let isSpotlightActive = window.innerWidth > 768; // Change the threshold as needed
 
-    spotlightDiv.addEventListener('mousemove', function (e) {
-        const rect = this.getBoundingClientRect();
-        const x = e.clientX - rect.left;
-        const y = e.clientY - rect.top;
+    function updateSpotlightPosition(e) {
+        if (isSpotlightActive) {
+            const rect = spotlightDiv.getBoundingClientRect();
+            const x = e.clientX - rect.left;
+            const y = e.clientY - rect.top;
 
-        this.style.setProperty('--cursor-x', `${x}px`);
-        this.style.setProperty('--cursor-y', `${y}px`);
+            spotlightDiv.style.setProperty('--cursor-x', `${x}px`);
+            spotlightDiv.style.setProperty('--cursor-y', `${y}px`);
+        }
+    }
+
+    function toggleSpotlightEffects() {
+        // Toggle both spotlight effect and darkening effect
+        if (isSpotlightActive) {
+            spotlightOverlay.style.display = 'block';
+        } else {
+            spotlightOverlay.style.display = 'none';
+        }
+
+        // You can add additional logic or styles here based on the state
+        if (isSpotlightActive) {
+            spotlightDiv.classList.add('spotlight-active');
+        } else {
+            spotlightDiv.classList.remove('spotlight-active');
+        }
+    }
+
+    spotlightDiv.addEventListener('mousemove', updateSpotlightPosition);
+
+    toggleButton.addEventListener('click', function () {
+        isSpotlightActive = !isSpotlightActive;
+        toggleSpotlightEffects();
     });
-});
 
+    // Listen to window resize events to toggle the spotlight effect based on screen width
+    window.addEventListener('resize', function () {
+        isSpotlightActive = window.innerWidth > 768; // Change the threshold as needed
+        toggleSpotlightEffects();
+    });
+
+    // Initial setup
+    toggleSpotlightEffects();
+});
