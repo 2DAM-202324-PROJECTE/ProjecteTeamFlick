d1760e5f779595c1257fdc3baa7d9a97bd570ce0
SergioBertomeu <sbertomeu1@iesebre.com>
2024-01-18 19:21:49 +0100

    llevar dependencies en mig de HTML

--- main.js
+++ main.js
@@ -64,19 +64,6 @@ document.addEventListener('DOMContentLoaded', function () {
 function toggleFAQ(faqId) {
     var element = document.getElementById(faqId);
     element.classList.toggle("hidden");
-}
+};
 
 
-let currentOpenContentId = null;
-
-function toggleContent(elementId) {
-    const content = document.getElementById(elementId);
-
-    if (currentOpenContentId && currentOpenContentId !== elementId) {
-        const currentOpenContent = document.getElementById(currentOpenContentId);
-        currentOpenContent.classList.add('hidden');
-    }
-
-    content.classList.toggle('hidden');
-    currentOpenContentId = content.classList.contains('hidden') ? null : elementId;
-}
