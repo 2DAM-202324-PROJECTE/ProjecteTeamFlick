fce558d23874a3d1dacab2777f399a3b47223bbe
«sara21h» <sbogdan@iesebre.com>
2024-01-23 20:29:09 +0100

    FIX #48

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,25 +2,20 @@
 export default {
   content: ["./**/*.{html,js}"],
   theme: {
-    extend: {},
-    screens: {
-      'xs': '400px',
-      'sm': '640px',
-      // => @media (min-width: 640px) { ... }
-
-      'md': '768px',
-      // => @media (min-width: 768px) { ... }
-
-      'lg': '1024px',
-      // => @media (min-width: 1024px) { ... }
-
-      'xl': '1280px',
-      // => @media (min-width: 1280px) { ... }
-
-      '2xl': '1536px',
-      // => @media (min-width: 1536px) { ... }
-      '3xl': '2000px'
-    }
+    extend: {
+      screens: {
+        'xs': '400px',
+        'sm': '640px',
+        'md': '768px',
+        'lg': '1024px',
+        'xl': '1280px',
+        'xl.5': '1520px',
+        '2xl': '1536px',
+        '3xl': '2000px'
+      },
+      spacing: {
+        '500': '1100px',
+      },
+    },
   },
 }
-
--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,16 @@
-export default {
-    // config options
-    base: '/ProjecteTeamFlick/'
-}
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
+export default defineConfig({
+    base: '/ProjecteTeamFlick/',
+    build: {
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, 'index.html'),
+                login: resolve(__dirname, 'login.html'),
+                signup: resolve(__dirname, 'register.html'),
+                cookies: resolve(__dirname, 'PoliticaCookies.html'),
+                privacitat: resolve(__dirname, 'PoliticaPrivacitat.html'),
+            },
+        },
+    },
+})
